c480e183b8e11551b1d5273ad80d7022655def54
dnhushak <dnhushak@pinnacle-recording.com>
2015-12-02 23:56:29 -0600

    Added logging and a spectrograph png feature

--- aircheck.sh
+++ aircheck.sh
@@ -8,7 +8,10 @@
 #
 #######################
 
-#File type
+#Log File
+LOG="$(dirname "$0")/aircheck.log"
+
+#File type to record to
 FILETYPE='mp3'
 
 #The filename to record to, formatted as "YYYYMMDD HHMMPM Aircheck.mp3"
@@ -24,25 +27,46 @@ OUTDIR="/Users/dnhushak/Desktop/Airchecks/"
 OUTDIR="$OUTDIR$(date '+20%y%m%d')/"
 
 #The duration of the recording (in hh:mm:ss format, or simply a numeric seconds format)
-DURATION='1'
+DURATION='5'
 
 ####################
 ##  SCRIPT START  ##
 ####################
 
+#Log File Formatting
+echo =============================== >> "$LOG"
+echo "" >> "$LOG"
+echo "Aircheck Recording Starting..." >> "$LOG"
+date >> "$LOG"
+echo "" >> "$LOG"
+
 #Generate output file, with the directory
 OUTFILE="$RECDIR$FILENAME"
+
+echo "Recording to $OUTFILE" >> "$LOG"
 
 #Record the file to the output directory and file, based on the duration
 # -d : use the default sound device for an input stream
 # trim : trim the "file" (in this case a stream) from a starting point to an end point
-sox -d "$OUTFILE" trim 0 $DURATION 
+sox -e s -d "$OUTFILE" trim 0 $DURATION 2>>"$LOG"
+sox "$OUTFILE" -n spectrogram -o "$OUTFILE.png" 2>>"$LOG"
 
 #Check if the desired output directory exists...
 if [[ ! -d "$OUTDIR" ]]; then
+	echo "Creating date directory $OUTDIR" >> "$LOG"
 	#If it doesn't, make the directory
-	mkdir "$OUTDIR"
+	mkdir "$OUTDIR" >> "$LOG"
 fi
 
+
 #And move all of the files to said directory
-mv -f "$RECDIR$FILENAME" "$OUTDIR"
+echo "Moving files" >> "$LOG"
+mv -f "$RECDIR$FILENAME" "$OUTDIR" >> "$LOG"
+mv -f "$RECDIR$FILENAME.png" "$OUTDIR" >> "$LOG"
+
+#Log File Closing
+echo "" >> "$LOG"
+date >> "$LOG"
+echo "Complete" >> "$LOG"
+echo "" >> "$LOG"
+exit
