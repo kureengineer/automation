ada3389b3dbaf853a75ceb64b236e691fbdc7144
dnhushak <dnhushak@pinnacle-recording.com>
2016-03-15 12:42:12 -0500

    added old windows batch files in archive

---
+++ Archives/Update Playlists.bat
@@ -0,0 +1,2 @@
+cd C:\Program Files\itunesexport
+java -jar itunesexport.jar -library="\\KURE-Library\KURE\KURE Library\iTunes Library.xml" -outputDir="\\KURE-Library\KURE\Playlists" -fileTypes=ALL -includePlaylist="High Priority,Medium Priority,Low Priority,Reco High,Reco Medium,M-Shop"
---
+++ Archives/Update Torq.bat
@@ -0,0 +1,9 @@
+copy "\\KURE-Library\KURE\Playlists" "C:\Program Files\itunesexport\plist"
+cd C:\Program Files\itunesexport
+fart plist\*.m3u #EXTM3U  #Playlist:
+fart plist\*.m3u Kure-library Volumes
+fart plist\*.m3u Kure-Library Volumes
+fart plist\*.m3u \\ *****/
+fart plist\*.m3u  \ *****/
+fart plist\*.m3u ***** --remove
+copy "C:\Program Files\itunesexport\plist" "\\KURE-Library\KURE\Torq Playlists"
